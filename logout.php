31e5f24429bab04751b6c3fb145ae34e745a3aba
Helmut Bierbaumer <helmut.bierbaumer@edu.fh-joanneum.at>
2017-04-22 06:51:52 +0200

    modified logout.php to use session data

--- logout.php
+++ logout.php
@@ -7,14 +7,17 @@
  * This page lets the user logout.
  */
 
+session_start();
+
 //If no cookie is presetn, redirect the user
-if (!isset($_COOKIE['user_id'])) {
+if (!isset($_SESSION['user_id'])) {
     //Need the function
     require('includes/login_function.inc.php');
     redirect_user();
-} else {
-    setcookie('user_id', '', time() - 3600, '/', '', 0, 0);
-    setcookie('first_name', '', time() - 3600, '/', '', 0, 0);
+} else { //Cancel the session
+    $_SESSION = array();    //Clear the variables
+    session_destroy();      //Destroy the session itself
+    setcookie('PHPSESSID', '', time() - 3600, '/', '', 0, 0);  //Destroy the session cookie
 }
 
 //Set the page title and include the HTML header
@@ -23,7 +26,7 @@ include('includes/header.html');
 
 //Print custominzed message
 echo "<h1>Logged Out!</h1>
-<p>You are now logged out, {$_COOKIE['first_name']}</p>";
+<p>You are now logged out</p>";
 
 include('includes/footer.html');
 ?>
