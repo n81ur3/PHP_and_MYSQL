2a63d38a3fdd7286b9df6bff0ee43e253e62d5b2
Helmut Bierbaumer <helmut.bierbaumer@edu.fh-joanneum.at>
2017-05-01 07:10:40 +0200

    created XSS example

--- first.php
+++ first.php
@@ -1,5 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<title>Basic PHP Page</title>
@@ -8,6 +7,10 @@
 	<!-- Script 1.2 - first.php -->
 	<p>This is standard HTML.</p>
 <?php
+for ($i = 0; $i < 10; $i++) {
+    system('date +%Y-%m-%d');
+    echo '<br />';
+}
 ?>
 </body>
 </html>
